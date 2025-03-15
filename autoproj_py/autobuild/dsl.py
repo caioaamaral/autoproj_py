6f1afb147c948c252020c379f8112bffcbc4c030
caioaamaral <caioaamaral@gmail.com>
2025-03-15 09:37:02 -0300

    feat: add dls extension decorator

--- autoproj_py/autobuild/dsl.py
+++ autoproj_py/autobuild/dsl.py
@@ -1,9 +1,16 @@
+import sys
+
 from autoproj_py.autobuild.registry import PackageRegistry
 from autoproj_py.autobuild.package import Package
 from autoproj_py.autobuild.packages.cmake import CMake
 
 
-def package(name: str, source: str):
+def extension(func):
+    setattr(sys.modules[__name__], func.__name__, func)
+    return func
+
+
+def import_package(name: str, source: str):
     pkg = Package(name, source)
     PackageRegistry.send(pkg.name, pkg)
 
