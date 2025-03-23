b370849309fe471f3dcad9134d3a29558ff88fc2
caioaamaral <caioaamaral@gmail.com>
2025-03-23 17:25:09 -0300

    feat: make Config work with nested namespaces

--- autoproj_py/config.py
+++ autoproj_py/config.py
@@ -5,15 +5,36 @@ class Config:
         self.config_file = config_file
         self.config = self.load_config()
 
-    def load_config(self):
+    def load_config(self) -> dict:
         with open(self.config_file, 'r') as f:
             return yaml.safe_load(f)
 
-    def get(self, key):
-        return self.config[key]
+    def get(self, key: str, fallback=None):
+        keys = key.split('.')
+        conf: dict = self.config.get(keys.pop(0))
 
-    def set(self, key, value):
-        self.config[key] = value
+        for key in keys:
+            if conf is None:
+                return fallback
+
+            conf = conf.get(key, None)
+
+        return conf
+
+    def set(self, key: str, value):
+        keys = iter(key.split('.'))
+
+        node = next(keys)
+        definition = { node: None }
+        current = definition
+        while (k := next(keys, None)):
+            current[node] = {k: None}
+            current = current[node]
+            node = k
+
+        current[node] = value
+        self.config.update(definition)
+
 
     def save(self):
         with open(self.config_file, 'w') as f:
