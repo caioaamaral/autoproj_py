a779d7598a31bfa6192ce34ffd1ce0a7701d551c
caioaamaral <caioaamaral@gmail.com>
2025-03-29 14:46:52 -0300

    feat: allow Autoproj to have acess to its environ

--- autoproj_py/environment.py
+++ autoproj_py/environment.py
@@ -1,7 +1,23 @@
+from pathlib import Path
+import shutil
+import subprocess
+
+from autoproj_py.subprocess import Subprocess
+
+
+def _validate_file(file: Path):
+    if not file.exists():
+        raise FileNotFoundError(f'{file} does not exist')
+    if not file.is_file():
+        raise FileNotFoundError(f'{file} is not a file')
+
+    return file
+
+
 class Environment:
 
-    def __init__(self, file):
-        self.file = file
+    def __init__(self, file: Path):
+        self.file = _validate_file(file)
         self._envars = dict()
         self._source_after = list()
 
@@ -16,6 +32,18 @@ class Environment:
 
     def source_after(self, file: str):
         self._source_after.append(file)
+
+    def export_context(self):
+        command = f'{shutil.which("bash")} -c ". {self.file} && env"'
+        p = Subprocess.run(command, stdout=subprocess.PIPE)
+        p.wait()
+
+        _context = dict()
+        for line in p.stdout:
+            key, value = line.strip().split('=', 1)
+            _context[key] = value
+
+        return _context
 
     def save(self):
         with open(self.file, 'w') as f:
