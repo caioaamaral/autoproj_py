58df246a36ff974872495b7f879b108c439dad62
Caio Amaral <caioaamaral@gmail.com>
2025-04-06 10:03:28 -0300

    feat: add common dsl to export packages definitions (#2)

--- autoproj_py/autobuild/dsl.py
+++ autoproj_py/autobuild/dsl.py
@@ -1,8 +1,9 @@
 import sys
 
-from autoproj_py.autobuild.registry import AutobuildCollector
-from autoproj_py.autobuild.packages.package import Package
 from autoproj_py.autobuild.packages.cmake import CMake
+from autoproj_py.autobuild.packages.package import Package
+from autoproj_py.autobuild.registry import AutobuildCollector
+from autoproj_py.vcs_definition import VCSDefinition
 
 
 def extension(func):
@@ -10,12 +11,24 @@ def extension(func):
     return func
 
 
-def import_package(name: str, source: str):
-    pkg = Package(name, source)
+def common_package(cls: type[Package], name: str, source: str|dict):
+    if isinstance(source, dict):
+        vcs = VCSDefinition.from_dict(source)
+    elif isinstance(source, str):
+        vcs = VCSDefinition.from_url(source)
+    else:
+        raise ValueError(f'Invalid source type: {type(source)}')
+
+    pkg = cls(name, vcs.url)
     AutobuildCollector.collect(pkg.name, pkg)
+    return pkg
+
+
+def import_package(name: str, source: str):
+    return common_package(Package, name, source)
 
 
 def cmake_package(name: str, source: str):
-    pkg = CMake(name, source)
+    pkg = common_package(CMake, name, source)
     pkg.dependencies = ['cmake']
-    AutobuildCollector.collect(pkg.name, pkg)
+    return pkg
